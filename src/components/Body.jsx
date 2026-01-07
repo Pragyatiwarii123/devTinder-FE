52fe63cf939e7e62c2024d70269a23da39c0297d
Pragya Tiwari <pragya.tiwari@fleetx.io>
2026-01-07 08:01:54 +0530

    if token expired redirect to login, should not access other route without login

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,34 @@
-import React from "react"
+import React, { useEffect } from "react"
 import NavBar from "./NavBar"
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom"
 import Footer from "./Footer"
+import axios from "axios"
+import { BASE_URL } from "../utils/constant"
+import { useDispatch, useSelector } from "react-redux"
+import { addUser } from "../reducers/userSlice"
 
 const Body = () => {
+    const dispatch = useDispatch()
+    const user = useSelector((store) => store.user)
+    const navigate = useNavigate()
+
+    const fetchUserProfile = async () => {
+        try {
+            const res = await axios.get(BASE_URL + "/profile/view", { withCredentials: true })
+            console.log(res, "rererrererererrere")
+            dispatch(addUser(res.data.data))
+        } catch (err) {
+            if(err.status === 401){
+                navigate("/login")
+            }
+        }
+    }
+
+    useEffect(() => {
+        if (!user) {
+            fetchUserProfile()
+        }
+    }, [])
     return (
         <>
             <NavBar />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     //subscribing to user store
@@ -8,7 +9,9 @@ const NavBar = () => {
     return (
         <div className="navbar bg-base-content shadow-sm">
             <div className="flex-1">
-                <a className="btn text-xl">👩‍💻 DevTinder</a>
+                <Link to={"/"}>
+                    <a className="btn text-xl">👩‍💻 DevTinder</a>
+                </Link>
             </div>
             {user && <div className="flex gap-2">
                 <div className="dropdown dropdown-end">
@@ -24,10 +27,10 @@ const NavBar = () => {
                         tabIndex="-1"
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
                         <li>
-                            <a className="justify-between">
+                            <Link to={'/profile'}>
                                 Profile
                                 <span className="badge">New</span>
-                            </a>
+                            </Link>
                         </li>
                         <li><a>Settings</a></li>
                         <li><a>Logout</a></li>
